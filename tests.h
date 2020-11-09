a5b442d392dd58eb18326ebc38aa356d645d6d12
Swen E Gaudl <swen.gaudl@plymouth.ac.uk>
2020-11-09 15:46:01 +0000

    last but one regular test change

--- tests.h
+++ tests.h
@@ -124,6 +124,7 @@ TEST_CASE("STD 11: Opening an invalid File from data folder") {
     FileReader reader = FileReader();
     //wrong file name supplied
     CHECK(reader.openFile("data/simples.dae") == false);
+    CHECK(reader.readFile("data/simples.dae") == false);
 }
 
 TEST_CASE("STD 12: Checking repeated Operations") {
@@ -131,13 +132,146 @@ TEST_CASE("STD 12: Checking repeated Operations") {
 
     reader.openFile("data/simple.dae");
     CHECK(reader.readFile("data/simple.dae") == true);
+    CHECK(reader.readFile("data/simple.dae") == true);
     reader.closeFile("data/simple.dae");
     CHECK(reader.currentFile().empty());
     CHECK(reader.getLines(0, 1).size() == 0);
 }
 
+TEST_CASE("STD 13: Multiple Files Can be Loaded and Inspected for their Elements") {
+    FileReader reader = FileReader();
+
+    //inspecting first file
+    reader.openFile("data/simple.dae");
+    reader.readFile("data/simple.dae");
+    std::string value = reader.inspectForFirstAttribute("instance_geometry", "url");
+    std::string comparison = "#Box_Mesh_345357280_002-mesh";
+    CHECK(value.compare(comparison) == 0); //going beyond the length of the file on purpose
+    value = reader.inspectForFirstAttribute("instance_geometry", "name");
+    comparison = "Box_Mesh_345357280.002";
+    CHECK(value.compare(comparison) == 0);
+
+
+    //inspecting second file
+    reader.openFile("data/advanced.dae");
+    reader.readFile("data/advanced.dae");
+    value = reader.inspectForFirstAttribute("geometry", "name");
+    comparison = "Cube.12_1.001";
+    CHECK(value.compare(comparison) == 0); 
+    std::vector<std::string> lines = reader.inspectForFirst("geometry", "technique_common");
+    REQUIRE(lines.size() > 1);
+    comparison = "  <accessor source=\"#Cube_12_1_001-mesh-positions-array\" count=\"8\" stride=\"3\">";
+    CHECK(lines[1].compare(comparison) == 0); 
+
+    CHECK(reader.closeFile("data/simple.dae"));
+    CHECK(reader.readFile("data/simple.dae") == false);
+    CHECK(reader.currentFile().compare("data/advanced.dae") == 0);
+    CHECK(reader.getLines(0, 10).size() == 11);
+}
+
+TEST_CASE("STD 14: Checking Large File") {
+    FileReader reader = FileReader();
+
+    REQUIRE(reader.openFile("data/large.dae"));
+    CHECK(reader.readFile("data/large.dae") == true);
+    std::string value = reader.inspectForFirstAttribute("instance_material", "target");
+    std::string comparison = "#Leather_pouf-material";
+    CHECK(value.compare(comparison) == 0);
+    reader.closeCurrentFile();
+}
+
+
+
+
 TEST_CASE("ADV 1: Trying to get a web-resource") {
     FileReader reader = FileReader();
     std::string fileName = "https://swen.fairrats.eu/research/webby.dae";
     CHECK(reader.openFile(fileName) == true);
+    CHECK(reader.readFile(fileName) == true);
+
+
+}
+TEST_CASE("ADV 2: Checking repeated Operations 2") {
+    FileReader reader = FileReader();
+
+    reader.openFile("data/simple.dae");
+    CHECK(reader.readFile("data/simple.dae") == true);
+    reader.closeFile("data/simple.dae");
+    CHECK(reader.currentFile().empty());
+    CHECK(reader.openFile("data/simple.dae") == true);
+    CHECK(reader.openFile("data/simple.dae") == false);
+    CHECK(reader.currentFile().compare("data/simple.dae") == 0);
+    CHECK(reader.getLines(0, 187)[100].compare("            </index_of_refraction>") == 0);//FIXIT!
+}
+
+TEST_CASE("ADV 3: Checking repeated Operations 3") {
+    FileReader reader = FileReader();
+
+    reader.openFile("data/simple.dae");
+    CHECK(reader.openFile("data/advanced.dae") == true);
+    CHECK(reader.currentFile().compare("data/advanced.dae") == 0);
+    CHECK(reader.getLines(0, 1).size() > 0);
+}
+
+TEST_CASE("ADV 4: Checking repeated Operations 4") {
+    FileReader reader = FileReader();
+
+    reader.openFile("data/simple.dae");
+    reader.openFile("data/advanced.dae");
+    CHECK(reader.closeFile("data/simple.dae"));
+    CHECK(reader.currentFile().compare("data/advanced.dae") == 0);
+    CHECK(reader.getLines(0, 1).size() > 0);
+}
+
+TEST_CASE("ADV 5: Checking repeated Operations 5") {
+    FileReader reader = FileReader();
+
+    reader.openFile("data/simple.dae");
+    reader.openFile("data/advanced.dae");
+    CHECK(reader.closeFile("data/advanced.dae"));
+    CHECK(reader.currentFile().compare("data/simple.dae") == 0);
+    CHECK(reader.getLines(0, 1).size() > 0);
+}
+
+/*
+* The specific content check in this method will be potentiall be
+* updated a day before the deadline. But the calls will all stay the same.
+*/
+TEST_CASE("ADV 6: Trying to get a web-resource") {
+    FileReader reader = FileReader();
+
+
+    std::string urlFile = "https://swen.fairrats.eu/research/webby.dae";
+    CHECK(reader.openFile(urlFile) == true);
+    CHECK(reader.readFile(urlFile) == true);
+
+    std::string value = reader.inspectForFirstAttribute("effect", "id");
+    std::string comparison = "Material_004-effect";
+    CHECK(value.compare(comparison) == 0); 
+    value = reader.inspectForFirstAttribute("effect", "technique","sid");
+    comparison = "common";
+    CHECK(value.compare(comparison) == 0);
+    value = reader.inspectForFirstAttribute("geometry", "source", "id");
+    comparison = "Creeper-mesh-positions";
+    CHECK(value.compare(comparison) == 0);
+
+
+    //inspecting second file
+    reader.openFile("data/advanced.dae");
+    reader.readFile("data/advanced.dae");
+
+    value = reader.inspectForFirstAttribute("geometry", "name");
+    comparison = "Cube.12_1.001";
+    CHECK(value.compare(comparison) == 0);
+    std::vector<std::string> lines = reader.inspectForFirst("geometry", "technique_common");
+    REQUIRE(lines.size() > 1);
+    comparison = "  <accessor source=\"#Cube_12_1_001-mesh-positions-array\" count=\"8\" stride=\"3\">";
+    CHECK(lines[1].compare(comparison) == 0);
+
+    CHECK(reader.closeFile(urlFile));
+    CHECK(reader.readFile(urlFile) == false);
+    CHECK(reader.currentFile().compare("data/advanced.dae") == 0);
+
+    CHECK(reader.getLines(0, 10).size() == 11);
+
 }
